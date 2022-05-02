df4c1adb9687d5457d1060c5b89989d0a82fb31e
swostika61 <swostikalamichhane61@gmail.com>
2022-05-02 11:23:10 +0545

    initial commit

---
+++ form/src/components/SignUp.vue
@@ -0,0 +1,19 @@
+<template>
+  <form action="">
+    <label>email</label>
+    <input type="text" />
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+form {
+  width: 450px;
+  background: #fff;
+  margin:auto;
+  padding:20px
+}
+</style>
