106decebdda4eea5d87f078c1d0f8f1e03ab5298
swostika61 <swostikalamichhane61@gmail.com>
2022-05-02 11:47:35 +0545

    use of v-model for email

--- form/src/components/SignUp.vue
+++ form/src/components/SignUp.vue
@@ -1,19 +1,37 @@
 <template>
   <form action="">
-    <label>email</label>
-    <input type="text" />
+    <label>Email:</label>
+    <input type="text" required v-model="email" />
   </form>
+  <p>{{ email }}</p>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return { email: "" };
+  },
+};
 </script>
 
 <style>
 form {
   width: 450px;
   background: #fff;
-  margin:auto;
-  padding:20px
+  margin: auto;
+  padding: 30px 20px;
+  display: flex;
+  border-radius: 10px;
+  flex-direction: column;
+}
+label {
+  display: flex;
+  margin-bottom: 5px;
+  color: rgb(130, 125, 125);
+}
+input {
+  width: 80%;
+  padding: 6px 10px;
+  margin-bottom: 10px;
 }
 </style>
